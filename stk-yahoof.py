097662f39a2d27acb1657836c691beac0bb34572
Aadi <129511378+jyotiradityajadhav@users.noreply.github.com>
2025-08-31 15:32:49 +0530

    Update stk-yahoof.py

--- stk-yahoof.py
+++ stk-yahoof.py
@@ -4,6 +4,7 @@ stocks = ['RELIANCE.NS', 'TCS.NS', 'HDFCBANK.NS', 'INFY.NS', 'HINDUNILVR.NS',
           'ICICIBANK.NS', 'KOTAKBANK.NS', 'AXISBANK.NS', 'ITC.NS', 'SBIN.NS']
 
 print("Downloading data...")
-data = yf.download(stocks, period='6mo', auto_adjust=True)  # Remove ['Adj Close']
+data = yf.download(stocks, period='6mo', auto_adjust=True)  
 data.to_csv('my_stocks.csv')
+
 print("Done! Check 'my_stocks.csv' file")
